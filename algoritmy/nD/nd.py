de11da8c8e5f800eaa3c89895956596c6d14d32a
Eric <eric.dusart.2005@gmail.com>
2024-04-23 13:52:42 +0200

    aa

---
+++ algoritmy/nD/nd.py
@@ -0,0 +1,20 @@
+import random
+import numpy as np
+
+n = 5
+points = np.array()
+
+for i in range(n**3):
+    point = ()
+    list = []
+    for k in range(n+1):
+        list.append(random.random()*random.randint(0, 100))
+    point = tuple(list)
+    points.add(point)
+# print(points)
+
+def perimeter(V:set):
+    for i in V:
+        for k in V:
+            for u in range(n):
+                a = 9 #mogus
