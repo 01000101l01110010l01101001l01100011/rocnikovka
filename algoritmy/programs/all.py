562acd14065e97b2c79e996e35846f13e70a7979
Eric <eric.dusart.2005@gmail.com>
2024-04-26 16:40:25 +0200

    aaa

--- algoritmy/programs/all.py
+++ algoritmy/programs/all.py
@@ -8,9 +8,9 @@ import csv
 
 number_of_cycles = 5
 numbers = []
-for i in range(60):
+for i in range(32):
    numbers.append(i)
-for i in range(10):
+for i in range(20):
    numbers.remove(i)
 
 one_time = np.array([])
@@ -24,58 +24,58 @@ nd_five_list = []
 nd_ten_list = []
 nd_twt_list = []
 def all():
-  with open('data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    for n_points in numbers:
-      print(n_points)
-      # 1D
-      for k in range(number_of_cycles):
-        points = generate_random_points(n_points, 1)
-        before = time.time()
-        algorithmone(points)
-        after = time.time() - before
-        writer.writerows([["1D", "Points:", n_points, "Time:", after]])
-        
+  with open('data_2D.csv', 'w', newline='') as file_2d:
+    writer_2D = csv.writer(file_2d)
+    with open('data_1D.csv', 'w', newline='') as file_1d:
+      writer_1D = csv.writer(file_1d)
+      with open('data_nD_2D.csv', 'w', newline='') as file_nd:
+        writer_nd = csv.writer(file_nd)
+        with open('data_nD_10D.csv', 'w', newline='') as file_ndd:
+          writer_ndd = csv.writer(file_ndd)
+          with open('data_nD_20D.csv', 'w', newline='') as file_nddd:
+            writer_nddd = csv.writer(file_nddd)
+            for n_points in numbers:
+              print(n_points)
+              # 1D
+              for k in range(number_of_cycles):
+                points = generate_random_points(n_points, 1)
+                before = time.time()
+                algorithmone(points)
+                after = time.time() - before
+                writer_1D.writerows([["1D", "Points:", n_points, "Time:", after]])
+                
 
-      # 2D
-      for k in range(number_of_cycles):
-        points = generate_random_points(n_points, 2)
+              # 2D
+              for k in range(number_of_cycles):
+                points = generate_random_points(n_points, 2)
 
-        before = time.time()
-        algorithmtwo(points)
-        after = time.time() - before
-        writer.writerows([["2D", "Points:", n_points, "Time:", after]])
-
-
-      # nD
-      for k in range(number_of_cycles):
-        points = generate_random_points(n_points, 2)
-        before = time.time()
-        algorithmnd(points)
-        after = time.time() - before
-        writer.writerows([["nD - 2D", "Points:", n_points, "Time:", after]])
+                before = time.time()
+                algorithmtwo(points)
+                after = time.time() - before
+                writer_2D.writerows([["2D", "Points:", n_points, "Time:", after]])
 
 
-      for k in range(number_of_cycles):
-        points = generate_random_points(n_points, 5)
-        before = time.time()
-        algorithmnd(points)
-        after = time.time() - before
-        writer.writerows([["nD - 5D", "Points:", n_points, "Time:", after]])
+              # nD
+              for k in range(number_of_cycles):
+                points = generate_random_points(n_points, 2)
+                before = time.time()
+                algorithmnd(points)
+                after = time.time() - before
+                writer_nd.writerows([["nD - 2D", "Points:", n_points, "Time:", after]])
 
-      for k in range(number_of_cycles//2):
-        points = generate_random_points(n_points, 10)
-        before = time.time()
-        algorithmnd(points)
-        after = time.time() - before
-        writer.writerows([["nD - 10D", "Points:", n_points, "Time:", after]])
+              for k in range(number_of_cycles):
+                points = generate_random_points(n_points, 10)
+                before = time.time()
+                algorithmnd(points)
+                after = time.time() - before
+                writer_ndd.writerows([["nD - 10D", "Points:", n_points, "Time:", after]])
 
-      for k in range(number_of_cycles//4):
-        points = generate_random_points(n_points, 20)
-        before = time.time()
-        algorithmnd(points)
-        after = time.time() - before
-        writer.writerows([["nD - 20D", "Points:", n_points, "Time:", after]])
+              for k in range(number_of_cycles//2):
+                points = generate_random_points(n_points, 20)
+                before = time.time()
+                algorithmnd(points)
+                after = time.time() - before
+                writer_nddd.writerows([["nD - 20D", "Points:", n_points, "Time:", after]])
 
 
 # 20, 10 = 101.5 sekund
@@ -88,4 +88,4 @@ def nd_test():
   print(after)
 
 
-nd_test()
+all()
--- algoritmy/programs/nD.py
+++ algoritmy/programs/nD.py
@@ -1,47 +1,61 @@
 import numpy as np
 from itertools import combinations
 from scipy.spatial.distance import euclidean
-import math, time
+import math
+import time
+
 
 def generate_random_points(num_points, dimension):
     points = np.random.rand(num_points, dimension)
     return points
 
+
 def generate_subsets(points, subset_length):
-    subsets = combinations(points, subset_length)   # Všechny (n+1)-prvkové podmnožiny
+    # Všechny (n+1)-prvkové podmnožiny
+    subsets = combinations(points, subset_length)
     return subsets
+
 
 def algorithmnd(points):
     num_points = len(points)  # Počet bodů
+    point_indices = list(range(num_points))  # Indexy bodů
     n = len(points[0])   # Dimenze
-    subsets = generate_subsets(points, n+1) # Vytvoří všechny (n+1)-prvkové podmnožiny
+    # Vytvoří všechny (n+1)-prvkové podmnožiny
+    subsets = list(generate_subsets(point_indices, n+1))
+
+    # Vytvoří matici vzdáleností
+    distances = np.zeros((num_points, num_points))
+    for i in range(num_points):
+        for j in range(i+1, num_points):
+            distances[i, j] = euclidean(points[i], points[j])
+            distances[j, i] = distances[i, j]
+
     print("Generated subsets")
-    polytopes = [] # Seznam polytopů
+    polytopes = []  # Seznam polytopů
     podmo = time.time()
-    for i, subset in enumerate(subsets): # Pro podmnožinu (polytop)
+    for i, subset in enumerate(subsets):  # Pro podmnožinu (polytop)
         perimeter = 0
-        edges = generate_subsets(subset, 2) # Množina všech 2-prvkových podmnožin
+        # Množina všech 2-prvkových podmnožin
+        edges = generate_subsets(subset, 2)
         for edge in edges:
-            perimeter += euclidean(edge[0], edge[1]) # Délka hrany
-        print("Polytope",i, "of", math.comb(num_points, n+1))
-        polytopes.append((subset, perimeter)) # Přidá hranu do seznamu polytopů
+            perimeter += distances[edge[0], edge[1]]  # Délka hrany
+        print("Polytope", i, "of", math.comb(num_points, n+1))
+        # Přidá hranu do seznamu polytopů
+        current_points = [points[j] for j in subset]
+        polytopes.append((current_points, perimeter))
+    # Seřadí seznam polytopů podle jejich obvodu
     print(time.time()-podmo)
-    podmo = time.time()
-    sorted_polytopes = sorted(polytopes, key=lambda x: x[-1]) # Seřadí seznam polytopů podle jejich obvodu
-    print(time.time()-podmo)
+    sorted_polytopes = sorted(polytopes, key=lambda x: x[-1])
     print("Sorted polytopes")
     while len(sorted_polytopes) > 0:
-        polytope, distance = sorted_polytopes[0] # Polytop a jeho obvod
-        x1 = polytope[0] # První bod polytopu
-        differences = [] # Seznam rozdílů
-        for x in polytope[1:]: # Pro bod v TODO
-            difference = x - x1 # Rozdíl bodů
-            differences.append(difference) # Přidá rozdíl do seznamu rozdílů
-        matrix = np.vstack(differences) # Vytvoří matici z rozdílů
-        determinant = np.linalg.det(matrix) # Spočítá determinant
-        if determinant != 0: # Pokud je determinant nenulový, řešením je tento polytop
+        polytope, distance = sorted_polytopes[0]  # Polytop a jeho obvod
+        x1 = polytope[0]  # První bod polytopu
+        differences = []  # Seznam rozdílů
+        for x in polytope[1:]:  # Pro bod v TODO
+            difference = x - x1  # Rozdíl bodů
+            differences.append(difference)  # Přidá rozdíl do seznamu rozdílů
+        matrix = np.vstack(differences)  # Vytvoří matici z rozdílů
+        determinant = np.linalg.det(matrix)  # Spočítá determinant
+        if determinant != 0:  # Pokud je determinant nenulový, řešením je tento polytop
             return polytope, distance
         print("Checked polytope")
-
-l = np.array([[1, 2, 3], [3, 2, 1], [4, 7, 0], [9, 5, 2], [2, 4, 6]])
-print(algorithmnd(l))
